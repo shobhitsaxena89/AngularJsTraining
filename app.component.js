0a850af94307e5803dbee301b7acb1f96e4cd746
shobhitsaxena89 <shobhitsaxena_89@yahoo.co.in>
2018-02-13 20:30:55 -0800

    commit 4

---
+++ app.component.js
@@ -0,0 +1,17 @@
+var myApp = angular.module('myApp');
+myApp.component('studentDetails', { 
+    //template: '<div>Student Details</div>'
+    templateUrl: 'student-details.tpl.html',
+    bindings : {
+      myTitle: '@' , //string value
+      //myList: '<' // it means we are passing property whcih can be an object or string
+      currentStudent: '<'
+    },
+    controllerAs: 'vm',
+    controller: ['$element', function($element){
+        //console.log($element);// $element is giving <div> from student details template.
+        // it's not a native element rather it's a jquerry wrapped element (if not jquerry then angular has jqlite) 
+        $element.css("color", "red") // not a good 
+        this.myname = 'custom component'
+    }]
+});
